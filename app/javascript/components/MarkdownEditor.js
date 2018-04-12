24709036e6e032342e1e33710454496c4220e38c
mmendesc <matheus.mendescf@gmail.com>
2018-04-12 18:40:58 -0300

    Markdown editor

---
+++ app/javascript/components/MarkdownEditor.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+import ReactMde, { ReactMdeTypes } from "react-mde";
+
+class MarkdownEditor extends React.Component {
+  render () {
+    return (
+      </ReactMde>
+    );
+  }
+}
+
+export default MarkdownEditor
