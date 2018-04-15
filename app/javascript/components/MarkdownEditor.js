7214d387004fe5b2339fcecdee94628e5deaf715
mmendesc <matheus.mendescf@gmail.com>
2018-04-15 00:07:25 -0300

    Rendered react-mde without style

--- app/javascript/components/MarkdownEditor.js
+++ app/javascript/components/MarkdownEditor.js
@@ -1,11 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
 import ReactMde, { ReactMdeTypes } from "react-mde";
+import * as Showdown from "showdown";
 
 class MarkdownEditor extends React.Component {
   render () {
     return (
-      </ReactMde>
+      <ReactMde/>
     );
   }
 }
